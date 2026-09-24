9c71571b6b2a97ba2929fc932d2a5fc8c4d8e25b
Dilip Kumar <dilip.sharma4558@gmail.com>
2026-09-24 16:40:40 +0530

    cors updated

--- Backend/index.js
+++ Backend/index.js
@@ -17,10 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-const corsOption={
-    origin:'http://localhost:5173',
-    credentials:true
-}
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://career-kart-phi.vercel.app",
+  ],
+  credentials: true,
+}));
 
 app.use(cors(corsOption));
 
